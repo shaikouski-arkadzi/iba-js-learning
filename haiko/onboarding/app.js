4e3e32633a81041d71d8bb220535f111a2f82b68
Andrei Haiko <andreygayko@gmail.com>
2020-09-08 22:40:35 +0300

    Add instruction form

---
+++ haiko/onboarding/app.js
@@ -0,0 +1,32 @@
+const DOMStrings = {
+    richtext: '.richtext',
+    btnAdd: 'btn-add_step',
+    btnSubmit: 'btn-submit_step',
+    editor: 'editor',
+    listSteps: 'list_steps'
+}
+
+const result = {
+    name: '',
+    steps: []
+}
+
+function submitStep() {
+    result.steps.push(document.getElementsByTagName('p')[1].innerHTML);
+    
+    const node = document.createElement("li");
+    node.innerHTML = result.steps[result.steps.length - 1];
+    document.getElementById(DOMStrings.listSteps).appendChild(node);
+
+    document.querySelector(DOMStrings.richtext).style.display = 'none';
+    document.getElementById(DOMStrings.btnAdd).style.display = 'block';
+}
+
+function addStep() {
+    document.querySelector(DOMStrings.richtext).style.display = 'block';
+    document.getElementById(DOMStrings.btnAdd).style.display = 'none';
+}
+
+function saveInstruction() {
+    result.name = 1;
+}
