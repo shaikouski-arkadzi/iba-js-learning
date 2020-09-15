550b0ea1252e310c2092967f534bb5ec486cfb7f
Andrei Haiko <andreygayko@gmail.com>
2020-09-15 12:21:44 +0300

    Add possibility to change instruction name

--- haiko/onboarding/app.js
+++ haiko/onboarding/app.js
@@ -4,7 +4,11 @@ const DOMStrings = {
     btnSubmit: 'btn-submit_step',
     editor: 'editor',
     listSteps: 'list_steps',
-    stepTitle: 'step_title'
+    stepTitle: 'step_title',
+    btnInstrName: 'change_instruction_name', 
+    instrNameInput: 'instruction_name_input',
+    instrChangeName: 'instruction_change_name',
+    btnSubmitInstrName: 'btn-submit_instruction_name'
 }
 
 const result = {
@@ -15,7 +19,6 @@ const result = {
 function submitStep() {
     const tit = document.getElementById(DOMStrings.stepTitle).value;
     const content = document.getElementsByTagName('p')[1].innerHTML;
-    console.log(tit);
 
     if(content && content !== '<br>' && tit) {
         
@@ -41,6 +44,28 @@ function addStep() {
     document.querySelector(DOMStrings.richtext).style.display = 'block';
     document.getElementById(DOMStrings.btnAdd).style.display = 'none';
 }
+
+document.getElementById(DOMStrings.btnInstrName).addEventListener('click', () => {
+    const title = document.getElementsByTagName('h1')[0];
+    const input = document.getElementById(DOMStrings.instrNameInput);
+    input.value = title.textContent;    
+
+    title.style.display = 'none';
+    input.style.display = 'inline-block';
+    document.getElementById(DOMStrings.btnSubmitInstrName).style.display = 'inline-block';
+    document.getElementById(DOMStrings.btnInstrName).style.display = 'none';
+})
+
+document.getElementById(DOMStrings.btnSubmitInstrName).addEventListener('click', () => {
+    const input = document.getElementById(DOMStrings.instrNameInput);
+    const title = document.getElementsByTagName('h1')[0];
+    title.textContent = input.value;
+
+    title.style.display = 'inline-block';
+    input.style.display = 'none';
+    document.getElementById(DOMStrings.btnSubmitInstrName).style.display = 'none';
+    document.getElementById(DOMStrings.btnInstrName).style.display = 'inline-block';
+})
 
 function saveInstruction() {
     result.name = 1;
